9b0454b1b78a3b00b1fe5dde1df3e70cb0b4b7b5
Dmitry Ivanov <dimannisik1@gmail.com>
2023-04-05 22:53:18 +0700

    feat: add title and lines

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import styles from './Root.module.scss';
 
 export default function Root() {
   return (
     <div className='container'>
-      <main>
+      <main className={styles.root}>
         <Outlet />
       </main>
     </div>
