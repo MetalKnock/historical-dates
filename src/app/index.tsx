d77adafc37fcd0197b552c69c05d887e4b74f131
Dmitry Ivanov <dimannisik1@gmail.com>
2023-04-13 05:57:38 +0700

    refactor: remove unused styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,6 @@ import { router } from '../pages';
 import './styles/index.scss';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-// import 'swiper/swiper-bundle.min.css';
 import 'swiper/css/navigation';
 import 'swiper/css/free-mode';
 
